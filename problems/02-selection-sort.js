037be49f87bf4588afe8323f2b5c1b374d248a5c
OByrnes <liviabyrnes@gmail.com>
2020-12-15 13:57:01 -0500

    problem 2

--- problems/02-selection-sort.js
+++ problems/02-selection-sort.js
@@ -2,13 +2,34 @@
 
 // Implement swap without looking at bubble sort
 function swap(arr, index1, index2) {
-  // your code here
+  [arr[index1],arr[index2]] = [arr[index2], arr[index1]]
+  
 }
+
+let list = [7,6,8,3,4,5,1,2,9,0]
 
 function selectionSort(list) {
   // your code here
+  for (let i=0; i< list.length; i++){
+    let minIndex = i
+    
+    for (let j=i+1; j< list.length; j++){
+      let ele = list[j]
+      
+      if (list[minIndex] > ele){
+        
+        minIndex=j
+      }
+      swap(list, i, minIndex)
+    }
+    
+  }
+  console.log(list)
+  return list
 }
 
+
+selectionSort(list)
 module.exports = {
   selectionSort,
   swap
