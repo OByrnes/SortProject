cae0cc8f0746fdded26a5fc6989a2dd7f2632fdf
B Salinas <b.salinas397@gmail.com>
2020-12-15 17:14:18 -0600

    Finished project and problem 5

--- problems/05-quick-sort.js
+++ problems/05-quick-sort.js
@@ -1,9 +1,35 @@
 // Implement Quick Sort
 
 function quickSort(array) {
-  // your code here
+  // base case
+  if (array.length <= 1) {
+    return array;
+  }
+  
+  // recursive case
+  let pivot = array[0];
+  let left = []; 
+  let right = []; 
+
+  array.forEach((ele) => {
+    if (ele < pivot) {
+      left.push(ele);
+    } else if (ele > pivot) {
+      right.push(ele);
+    }
+  });
+ 
+  // recursive step
+  let leftHalf = quickSort(left);
+  let rightHalf = quickSort(right);
+
+  return [...leftHalf, pivot, ...rightHalf];
 }
 
+let array = [1, 9, 2, 8, 3, 7, 4, 6, 5, 0];
+// quickSort(array);
+// console.log(array);
+console.log(quickSort(array));
 
 module.exports = {
   quickSort
