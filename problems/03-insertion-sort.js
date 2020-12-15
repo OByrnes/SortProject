0413d00f0ecd3d481a831a9bc3ca0a74cddde431
B Salinas <b.salinas397@gmail.com>
2020-12-15 13:15:44 -0600

    problem 3

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,8 +1,24 @@
 // Implement Insertion Sort
 
 function insertionSort(list) {
-  // your code here
+  let position = 1;
+
+  while (position < list.length) {
+    for (let i = position; i >= 0; i--) {
+
+      if (list[i] < list[i-1]) {
+        let deleted = list.splice(i-1, 2, list[i], list[i-1]);
+
+        console.log(deleted);
+      }
+    }
+    position++;
+  }
 }
+
+let array = [5, 4, 3, 2, 9, 1, 0, 6];
+insertionSort(array);
+console.log(array);
 
 module.exports = {
   insertionSort
